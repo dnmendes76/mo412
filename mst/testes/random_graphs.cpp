8eeb4769a78796c1a05d44652bb37d6621e734ab
João Paulo <joao.pkc@gmail.com>
2019-05-14 00:08:46 -0300

    Adicionando novos testes

--- mst/testes/random_graphs.cpp
+++ mst/testes/random_graphs.cpp
@@ -5,45 +5,52 @@
 
 using namespace std;
 
+typedef pair<int, int>ii;
+
 const int MAXV = 1123;
 
+const int MAXVV = 1123456;
+
 int MA[MAXV][MAXV];
-int pesos[MAXV];
+double pesos[MAXVV];
 
 int main(void) {
   int n, m, na;
+  set<ii> vts;
   memset(MA, 0, sizeof(MA));
   scanf("%d %d", &n, &m);
   printf("%d\n%d\n", n, m);
 
-
   //for (int i = 0; i < MAXV; i++) MA[i][i] = 1;
 
-  for (int i = 0; i < MAXV; i++) {
-    random_device dev;
-    mt19937 rng(dev());
-    uniform_int_distribution<std::mt19937::result_type> dist6(1, 200);
-    pesos[i] = dist6(rng);
-  }
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_real_distribution<> peso(1.0, 200.0);
+
+  for (int i = 0; i < MAXVV; i++) pesos[i] = peso(gen);
 
   for (int i = 1; i < n; i++) {
-    random_device dev;
-    mt19937 rng(dev());
-    uniform_int_distribution<std::mt19937::result_type> dist6(0, i - 1);
-    int tmp = dist6(rng);
-    printf("%d %d %d\n", i, tmp, pesos[i - 1]);
-    MA[i][tmp] = MA[tmp][i] = 1;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> vt(0, i - 1);
+
+    int tmp = vt(gen);
+    printf("%d %d %.3lf\n", i, tmp, pesos[i - 1]);
+    vts.insert(ii(i, tmp));
+    vts.insert(ii(tmp, i));
   }
 
   for (na = n - 1; na < m; ) {
-    random_device dev;
-    mt19937 rng(dev());
-    uniform_int_distribution<std::mt19937::result_type> dist6(0, n - 1);
-    int tmp1 = dist6(rng);
-    int tmp2 = dist6(rng);
-    if (!MA[tmp1][tmp2]) {
-      MA[tmp1][tmp2] = MA[tmp2][tmp1] = 1;
-      if (tmp1 != tmp2) printf("%d %d %d\n", tmp1, tmp2, pesos[na + 1]);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> vt(0, n - 1);
+
+    int tmp1 = vt(gen);
+    int tmp2 = vt(gen);
+    if(vts.find(ii(tmp1, tmp2)) != vts.end()) {
+      vts.insert(ii(tmp1, tmp2));
+      vts.insert(ii(tmp2, tmp1));
+      if (tmp1 != tmp2) printf("%d %d %.3lf\n", tmp1, tmp2, pesos[na + 1]);
       na++;
     }
   }
