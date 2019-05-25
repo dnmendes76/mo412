a878a23b30e3f8dd8875feb7017952ccfb124534
freddye <victor-araujo@alu.ufc.br>
2019-05-25 15:56:08 -0300

    MO412: Default Bellman-Ford Algorithm

---
+++ mst/sources/cmake-build-debug/CMakeFiles/sources.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sources.dir/src/filter_kruskal.cpp.o"
+  "CMakeFiles/sources.dir/src/grafo.cpp.o"
+  "CMakeFiles/sources.dir/src/heap_binary.cpp.o"
+  "CMakeFiles/sources.dir/src/kruskal.cpp.o"
+  "CMakeFiles/sources.dir/src/main.cpp.o"
+  "CMakeFiles/sources.dir/src/prim.cpp.o"
+  "CMakeFiles/sources.dir/src/union_find.cpp.o"
+  "CMakeFiles/sources.dir/src/union_find_2.cpp.o"
+  "CMakeFiles/sources.dir/src/union_find_3.cpp.o"
+  "CMakeFiles/sources.dir/src/union_find_4.cpp.o"
+  "CMakeFiles/sources.dir/src/bucket_sort.cpp.o"
+  "CMakeFiles/sources.dir/src/bucket.cpp.o"
+  "sources.pdb"
+  "sources"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sources.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- mst/sources/src/bucket_sort.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by carlos on 13/05/19.
-//
-
-#include "bucket_sort.h"
