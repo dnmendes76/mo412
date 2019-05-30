6b002a3efcd78ec2d72f4391c08fb1516fa99fcb
Daiane <dnmendes76@gmail.com>
2019-05-30 12:42:54 -0300

    Testando...

--- caminhos/sources/src/main.cpp
+++ caminhos/sources/src/main.cpp
@@ -4,24 +4,29 @@
 #include <floyd_warshall.h>
 #include <johnson.h>
 
-int main() {
+int main(int argc, char const *argv[]) {
+	if (argc != 5) {
+        printf("Informe 4 argumentos.\nEx.: djikstra 0 input.txt output.txt");
+        return 0;
+    }
+
     Graph * graph = new Graph();
-    graph->load_graph("toy_instance.txt");
+    graph->load_graph(argv[3]);
     BellmanFord bf = BellmanFord(graph);
     bf.solve();
-    bf.salve_soluction("bf.txt");
+    bf.salve_soluction("testes/outputs/bf.txt");
 
     Djikstra dj = Djikstra(graph);
     dj.solve();
-    dj.salve_soluction("dj.txt");
+    dj.salve_soluction("testes/outputs/dj.txt");
 
     FloydWarshall fw = FloydWarshall(graph);
     fw.solve();
-    fw.salve_soluction_all_pairs("fw.txt");
+    fw.salve_soluction_all_pairs("testes/outputs/fw.txt");
 
 	Johnson js = Johnson(graph);
     js.solve();
-    js.salve_soluction_all_pairs("js.txt");
+    js.salve_soluction_all_pairs("testes/outputs/js.txt");
 
     return 0;
 }
