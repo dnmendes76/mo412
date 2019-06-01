b6c012e2a7cab89b7431ce38f9d4c2bd27f2e957
Daiane Mendes de Oliveira <dnmendes76@gmail.com>
2019-06-01 10:31:07 -0300

    Padronizando input e output main

--- caminhos/sources/src/main.cpp
+++ caminhos/sources/src/main.cpp
@@ -16,54 +16,66 @@ int main(int argc, char const *argv[]) {
     int option = atoi(((string)argv[2]).c_str());
     string path_output = argv[4];
 
+    BellmanFord is_valid = BellmanFord(graph);
+    is_valid.solve();
+    if(is_valid.has_negative_cycle){
+        ofstream file;
+        file.open(path_output);
+        file << "Graph has negative cycle! :/" << endl;
+        cout << "Graph has negative cycle! :/" << endl;
+        file.close();
+        exit(0);
+    }
+
     if(alg[0].compare(argv[1]) == 0) { //bellman-ford
     	BellmanFord bf = BellmanFord(graph);
+        if(option == 0){
+            bf.solve();
+        } else if (option == 1) {
+            bf.solve_two();
+        } else {
+            cout << "Invalid Option!" << endl;
+        }
+        bf.salve_soluction(path_output);
 
     } else if(alg[1].compare(argv[1]) == 0) { //dijkstra
     	Dijkstra dj = Dijkstra(graph);
+        if(option == 0) {
+            dj.solve_vector();
+        } else if (option == 1) {
+            dj.solve_heap_binary();
+        } else if (option == 2) {
+            dj.solve_heap_fibonacci();
+        } else {
+            cout << "Invalid Option!" << endl;
+        }
+        dj.salve_soluction(path_output);
 
     } else if(alg[2].compare(argv[1]) == 0) { //floyd-warshall
     	FloydWarshall fw = FloydWarshall(graph);
+        if(option == 0) {
+            fw.solve();
+            fw.salve_soluction_all_pairs(path_output);
+        } else {
+            cout << "Invalid Option!" << endl;
+        }
 
     } else if (alg[3].compare(argv[1]) == 0) { // johnson
     	Johnson js = Johnson(graph);
+        if(option == 0) {
+            js.solve_dijkstra_vector();
+        } else if (option == 1) {
+            js.solve_dijkstra_heap_bin();
+        } else if (option == 2) {
+            js.solve_dijkstra_heap_fibo();
+        } else {
+            cout << "Invalid Option!" << endl;
+        }
+        js.salve_soluction_all_pairs(path_output);
 
     } else {
     	cout << "Sorry! This algorithm not implemented yet :|" << endl;
     }
-
-
-
-    BellmanFord bf = BellmanFord(graph);
-    bf.solve();
-    bf.salve_soluction("testes/outputs/bf.txt");
-    bf.solve_two();
-    bf.salve_soluction("testes/outputs/bf2.txt");
-
-    Dijkstra dj = Dijkstra(graph);
-    dj.solve_heap_binary();
-    dj.salve_soluction("testes/outputs/dj1.txt");
-
-    dj.solve_heap_fibonacci();
-    dj.salve_soluction("testes/outputs/dj2.txt");
-
-    dj.solve_vector();
-    dj.salve_soluction("testes/outputs/dj3.txt");
-
-    FloydWarshall fw = FloydWarshall(graph);
-    fw.solve();
-    fw.salve_soluction_all_pairs("testes/outputs/fw.txt");
-
-	Johnson js = Johnson(graph);
-    js.solve_dijkstra_vector();
-    js.salve_soluction_all_pairs("testes/outputs/js.txt");
-
-    js.solve_dijkstra_heap_bin();
-    js.salve_soluction_all_pairs("testes/outputs/js2.txt");
-
-    js.solve_dijkstra_heap_fibo();
-    js.salve_soluction_all_pairs("testes/outputs/js3.txt");
-
 
     return 0;
 }
