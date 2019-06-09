620dfb151c20a791d84cf99203061bef91427b8d
João Paulo Castilho <joao.pkc@gmail.com>
2019-06-09 14:22:33 -0300

    Alterando main para padrão da descrição do trabalho

--- fluxos/sources/src/main.cpp
+++ fluxos/sources/src/main.cpp
@@ -9,15 +9,22 @@ int main(int argc, char const *argv[]){
     return 0;
   }
 
+  string alg[4] = {"ford-fulkerson", "edmonds-karp"};
+
   Graph graph, soluction;
   graph.load_grafo(argv[2]);
   //graph.print_grafo();
 
-  Fluxo_Edmons_Karp ek(graph);
-  soluction = ek.solve();
-
-  Fluxo_Edmons_Karp fk(graph);
-  soluction = fk.solve();
+  if (alg[0].compare(argv[1]) == 0) { //Ford-Fulkerson
+    Fluxo_Edmons_Karp fk(graph);
+    soluction = fk.solve();
+  } else if (alg[1].compare(argv[1]) == 0) { //Edmons-Karp
+    Fluxo_Edmons_Karp ek(graph);
+    soluction = ek.solve();
+  } else {
+    cout << "Opcao Invalida" << endl;
+    return 0;
+  }
 
   Save salva_arquivo(soluction);
   salva_arquivo.save_soluction(argv[2], argv[3]);
