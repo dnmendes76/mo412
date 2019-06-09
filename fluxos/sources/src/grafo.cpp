a7908a7ecb0eb9ffcb9b26b9a19d6530dc2c27dd
João Paulo Castilho <joao.pkc@gmail.com>
2019-06-09 13:58:14 -0300

    Ajeitando montagem do grafo

--- fluxos/sources/src/grafo.cpp
+++ fluxos/sources/src/grafo.cpp
@@ -1,4 +1,4 @@
-#include "grafo.h"
+#include "../inc/grafo.h"
 
 void Graph::load_grafo(string path) {
   ifstream file;
@@ -12,14 +12,7 @@ void Graph::load_grafo(string path) {
       m_adj[i][j] = 0;
   for (int i = 0; i < m; i++) {
     file >> e.u >> e.v >> e.custo;
-    if (m_adj[e.v][e.u]) {
-      m_adj.resize(n + 1);
-      for (int j = 0; j < n; j++) m_adj[j].resize(n + 1, 0);
-      m_adj[n].resize(n + 1, 0);
-      m_adj[e.u][n] = m_adj[n][e.v] = e.custo;
-      n++;
-    }
-    else m_adj[e.u][e.v] += e.custo;
+    m_adj[e.u][e.v] += e.custo;
   }
   for (int i = 0; i < n; i++) {
     m_adj[i].resize(n, 0);
